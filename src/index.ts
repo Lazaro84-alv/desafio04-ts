49a6bfae4940b35c960e46cac9292d82eea75b62
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-11-11 00:21:23 -0300

    Separa as rotas de usuarios

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
+import { router } from './routes';
 import express, { Request, Response } from 'express';
-import { UserController } from './controllers/UserController';
-
-const userController = new UserController()
 
 const server = express();
 
 server.use(express.json());
+server.use(router)
 
 server.get('/', (request: Request, response: Response) => {
   return response.status(200).json({ message: 'DioBank API' })
 })
 
-server.post('/user', userController.createUser)
-server.get('/user', userController.getAllUsers)
+
 
 server.listen(3000, () => console.log('Server on'));
